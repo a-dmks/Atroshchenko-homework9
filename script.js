b77669c88e6b39764736158adbedbb92769eba49
a-dmks <dmitryatroshenko01@gmail.com>
2023-01-03 19:11:30 +0200

    homework9

---
+++ script.js
@@ -0,0 +1,19 @@
+
+const components = prompt('Введіть довжини катетів через пробіл')
+
+if (components) {
+    const cathetus = components.split(' ');
+    if (cathetus.length !== 2) {
+        alert('Помилка! Невірна кількість довжин');
+    } 
+    else if (isNaN(cathetus[0]) || isNaN(cathetus[1])) {
+        alert('Помилка! Ви вказали невалідні довжини');
+    } 
+    else {
+        alert(`Розраховано за допомогою Math: ${Math.hypot(cathetus[0], cathetus[1])}
+Розраховано без допомоги Math: ${(cathetus[0]**2 + cathetus[1]**2)**0.5}`);
+    }
+} 
+else {
+    alert('Ви не ввели довжини :(')
+}
